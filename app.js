b5ed41e4361a918f041fb1f99930a559b67a3a51
rwalker <robpub@robonco.com>
2019-11-02 18:16:12 -0500

    Serve bootstrap and jquery locally

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var path = require('path');
 var app = express();
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
