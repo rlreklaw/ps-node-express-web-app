856d9c27cfdce7d638522d4be551a94398057057
rwalker <robpub@robonco.com>
2019-11-02 17:40:04 -0500

    add express logging with morgan

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var chalk = require('chalk');
 var debug = require('debug')('app');
+var morgan = require('morgan');
 
 var app = express();
+
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
     res.send('Hello from my library app');
