ca919706e8c069f130470bba4f5e1f9790e24469
rwalker <robpub@robonco.com>
2019-11-02 18:31:13 -0500

    Serve jquery and bootstrap from node_modules  dir

--- app.js
+++ app.js
@@ -7,7 +7,10 @@ var path = require('path');
 var app = express();
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
