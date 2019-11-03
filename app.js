3c2ff45aba6b9e1a474eddf452061b786cd8bbe4
rwalker <robpub@robonco.com>
2019-11-03 09:28:47 -0600

    Add  stub routes for books

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,6 +15,18 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+
+bookRouter.route('/')
+  .get((req, res) => {
+    res.send('Hello books!');
+  });
+
+bookRouter.route('/single')
+  .get((req, res) => {
+    res.send('Hello single book!');
+  });
+
+app.use('/books', bookRouter);
 
 app.get('/', (req, res) => {
   res.render('index', {
