218f21e69eb50bfc2c8187c9e19364a67998ec82
rwalker <robpub@robonco.com>
2019-11-03 09:53:35 -0600

    Display list of books

--- app.js
+++ app.js
@@ -16,9 +16,64 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Miserables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'A Journey into the Center of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'Life On The Mississippi',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: false
+  },
+  {
+    title: 'Childhood',
+    genre: 'Biography',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  }];
+
 bookRouter.route('/')
   .get((req, res) => {
-    res.send('Hello books!');
+    res.render('books', {
+      title: 'Library',
+      books,
+      nav: [{ link: '/books', title: 'Books' },
+        { link: '/authors', title: 'Authors' }]
+    });
   });
 
 bookRouter.route('/single')
@@ -31,6 +86,7 @@ app.use('/books', bookRouter);
 app.get('/', (req, res) => {
   res.render('index', {
     title: 'Library',
+    books,
     nav: [{ link: '/books', title: 'Books' },
       { link: '/authors', title: 'Authors' }]
   });
