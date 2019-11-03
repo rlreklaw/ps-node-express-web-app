f8748bc6d6a9ad0893c821f235391fe0bb87426f
rwalker <robpub@robonco.com>
2019-11-03 06:24:34 -0600

    Fix lint problems

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
+const app = express();
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -13,9 +13,9 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
 app.listen(3000, () => {
-    debug(`listening on port ${chalk.green('3000')}`);
+  debug(`listening on port ${chalk.green('3000')}`);
 });
