6e72736ac87dd811c80cb5a127c3537a24f4679a
rwalker <robpub@robonco.com>
2019-11-05 07:33:26 -0600

    Add service to get book description

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,7 +1,7 @@
 const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('app:bookController');
 
-function bookController(nav) {
+function bookController(bookService, nav) {
 
   function getIndex(req, res) {
     const url = 'mongodb://localhost:27017';
@@ -45,6 +45,7 @@ function bookController(nav) {
         const col = await db.collection('books');
         const book = await col.findOne({ _id: new ObjectID(id) });
         debug(book);
+        book.details = await bookService.getBookById(book.bookId);
         res.render(
           'bookView',
           {
