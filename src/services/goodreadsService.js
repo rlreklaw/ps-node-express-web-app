ab5905cc43b6297816a5669276f325385f088e40
rwalker <robpub@robonco.com>
2019-11-05 08:41:33 -0600

    Retrieve book details from goodreads API
    Goodreads API returns XML for book details.
    Convert it to  JSON to make it easier to consume.

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,8 +1,26 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('app:goodreadsService');
 
+const parser = new xml2js.Parser({ explicitArray: false });
 function goodreadsService() {
-  function getBookById() {
+  function getBookById(id) {
     return new Promise((resolve, reject) => {
-      resolve({ description: 'our description' });
+      axios.get('https://www.goodreads.com/book/show/656.xml?key=rqzpQfjzEHchGjWVBNsw')
+        .then((response) => {
+          parser.parseString(response.data, (err, result) => {
+            if (err) {
+              debug(err);
+            } else {
+              debug(result);
+              resolve(result.GoodreadsResponse.book);
+            }
+          });
+        })
+        .catch((error) => {
+          reject(error);
+          debug(error);
+        });
     });
   }
 
