3a9bf9700bfcd8260d1f118cf9ff49430b871893
rwalker <robpub@robonco.com>
2019-11-04 18:43:27 -0600

    Insert books into MongoDB

--- app.js
+++ app.js
@@ -39,8 +39,10 @@ const nav = [{ link: '/books', title: 'Book' },
   { link: '/authors', title: 'Author' }];
 
 const bookRouter = require('./src/routes/bookRoutes')(nav);
+const adminRouter = require('./src/routes/adminRoutes')(nav);
 
 app.use('/books', bookRouter);
+app.use('/admin', adminRouter);
 
 app.get('/', (req, res) => {
   res.render('index', {
